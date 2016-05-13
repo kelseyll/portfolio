e918445aebb54b8eaac9ce323479111780698356
kelseyll <kelsey.legault@gmail.com>
2016-05-12 21:37:02 -0700

    Add subtle animation to footer links

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ $(".work-items-container a").hover(
     $(this).find("span:first-child").addClass("work-items-container-span-hover");
     $(this).find("span:nth-child(2n)").addClass("work-items-container-second-span-hover");
     $(this).find("span:nth-child(3n)").addClass("work-items-container-third-span-hover");
-    $(this).removeClass("work-item-mouseLeave-slideRight");
+    $(this).removeClass("slideRight");
   },
   function () {
     $(this).find("li").removeClass("work-items-container-li-hover");
     $(this).find("span:first-child").removeClass("work-items-container-span-hover");
     $(this).find("span:nth-child(2n)").removeClass("work-items-container-second-span-hover");
     $(this).find("span:nth-child(3n)").removeClass("work-items-container-third-span-hover");
-    $(this).addClass("work-item-mouseLeave-slideRight");
+    $(this).addClass("slideRight");
+  }
+);
+
+$(".footer-text-container li").hover(
+  function () {
+    $(this).find("i").addClass("slideLeftSm");
+    $(this).find("i").removeClass("slideRightSm");
+  },
+  function () {
+    $(this).find("i").removeClass("slideLeftSm");
+    $(this).find("i").addClass("slideRightSm");
   }
 );
