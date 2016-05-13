0507991306fa35be2236275e8da5bc56fc15514e
kelseyll <kelsey.legault@gmail.com>
2016-05-12 21:22:05 -0700

    Take away fade from work item animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
+/* Animations ----------------------------------- */
 $(".work-items-container a").hover(
   function () {
     $(this).find("li").addClass("work-items-container-li-hover");
     $(this).find("span:first-child").addClass("work-items-container-span-hover");
     $(this).find("span:nth-child(2n)").addClass("work-items-container-second-span-hover");
     $(this).find("span:nth-child(3n)").addClass("work-items-container-third-span-hover");
-    $(this).removeClass("work-item-mouseLeave-animation");
+    $(this).removeClass("work-item-mouseLeave-slideRight");
   },
   function () {
     $(this).find("li").removeClass("work-items-container-li-hover");
     $(this).find("span:first-child").removeClass("work-items-container-span-hover");
     $(this).find("span:nth-child(2n)").removeClass("work-items-container-second-span-hover");
     $(this).find("span:nth-child(3n)").removeClass("work-items-container-third-span-hover");
-    $(this).addClass("work-item-mouseLeave-animation");
+    $(this).addClass("work-item-mouseLeave-slideRight");
   }
 );
-// $( ".work-items-container a" ).mouseleave(function() {
-//     console.log("lol");
-//     $(this).addClass("work-item-mouseLeave-animation");
-// });
