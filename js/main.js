e87d219dc40f3552a00c3983ee7be4c790375973
kelseyll <kelsey.legault@gmail.com>
2016-05-15 00:06:05 -0700

    Refine hero animations

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ $(".work-items-container a").hover(
     $(this).find("i").addClass("slideLeftSm");
     $(this).find("i").removeClass("slideRightSm");
     $(this).find("span:nth-child(2n)").addClass("work-items-container-second-span-hover");
+    $(this).find("span:nth-child(3n)").addClass("slideLeftSm");
+    $(this).find("span:nth-child(3n)").removeClass("slideRightSm");
   },
   function () {
     $(this).find("i").removeClass("slideLeftSm");
     $(this).find("i").addClass("slideRightSm");
     $(this).find("span:nth-child(2n)").removeClass("work-items-container-second-span-hover");
+    $(this).find("span:nth-child(3n)").removeClass("slideLeftSm");
+    $(this).find("span:nth-child(3n)").addClass("slideRightSm");
+
   }
 );
 $(".footer-text-container li").hover(
