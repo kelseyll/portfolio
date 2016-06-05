dcf5e5942124651dcead21979cee4d9160e1e9c8
kelseyll <kelsey.legault@gmail.com>
2016-06-05 13:40:55 -0700

    Add tunes and shh button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(function() {
-  $( ".drag" ).draggable();
+$( document ).ready(function() {
+
+    // Load Music
+    var bgMusic = new buzz.sound("sound/kmart2_audition", {
+        formats: [ "mp3" ],
+        preload: true,
+        autoplay: true,
+        loop: true,
+        volume: 30
+    });
+
+    // Mute/Play Button
+    $( ".volume-button" ).click(function() {
+        $( "span" ).toggle();
+        bgMusic.togglePlay();
+    });
+
+    // Dragglable elements
+    $(function() {
+      $( ".drag" ).draggable();
+    });
 });
